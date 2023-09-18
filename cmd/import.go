cd80dbc4f18325891f658a1d6400926f332202d9
webws <websong@websongdeMacBook-Pro.local>
2023-09-19 00:10:38 +0800

    cmd import

--- cmd/import.go
+++ cmd/import.go
@@ -1,9 +1,20 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/spf13/cobra"
+	"github.com/webws/embedding-knowledge-base/pkg/qdrant"
 	"github.com/webws/go-moda/logger"
+
+	pb "github.com/qdrant/go-client/qdrant"
 )
+
+type QA struct {
+	Questions string `json:"questions" bson:"questions"`
+	Answers   string `json:"answers" bson:"answers"`
+}
 
 var importCmd = &cobra.Command{
 	Use:   "import",
@@ -12,12 +23,53 @@ var importCmd = &cobra.Command{
 
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// TODO:flg to conf
-		// dataFile, err := cmd.Flags().GetString(dataFileFlag)
-		// if err != nil {
-		// 	logger.Errorw("importCmd GetString fail", "err", err)
-		// 	return err
-		// }
 		logger.Infow("import cmd")
-		return nil
+		qdrantAddr, err := cmd.Flags().GetString(qdrantFlag)
+		if err != nil {
+			return err
+		}
+		qdrantClient := qdrant.NewQdrantClient(qdrantAddr)
+		defer qdrantClient.Close()
+
+		if err = qdrantClient.CreateCollection("kubernetes", 1536); err != nil {
+			return err
+		}
+		// TODO config to QA
+		var qas []*QA
+		points := []*pb.PointStruct{}
+		for _, qa := range qas {
+			// TODO openai CreateEmbeddings
+			var embedding []float32
+			point := buildPoint(qa.Questions, qa.Answers, embedding)
+			points = append(points, point)
+		}
+		return qdrantClient.CreatePoints("kubernetes", points)
 	},
+}
+
+func buildPoint(question string, answers string, embedding []float32) *pb.PointStruct {
+	point := &pb.PointStruct{}
+	// point id
+	uuid := fmt.Sprintf("%s-%d", question, time.Now().UnixNano())
+	point.Id = &pb.PointId{
+		PointIdOptions: &pb.PointId_Uuid{
+			Uuid: uuid,
+		},
+	}
+
+	// vector
+	point.Vectors = &pb.Vectors{
+		VectorsOptions: &pb.Vectors_Vector{
+			Vector: &pb.Vector{
+				Data: embedding,
+			},
+		},
+	}
+
+	// payload
+	ret := make(map[string]*pb.Value)
+	ret["question"] = &pb.Value{Kind: &pb.Value_StringValue{StringValue: question}}
+	ret["answers"] = &pb.Value{Kind: &pb.Value_StringValue{StringValue: answers}}
+	point.Payload = ret
+	return point
 }
