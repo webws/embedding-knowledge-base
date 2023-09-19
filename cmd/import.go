f5a0675dded15f788542487bd42cb0d1f147e3e3
wangsong <wangsong@hellotalk.com>
2023-09-19 17:19:50 +0800

    ai method

--- cmd/import.go
+++ cmd/import.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/webws/embedding-knowledge-base/pkg/ai"
 	"github.com/webws/embedding-knowledge-base/pkg/qdrant"
 	"github.com/webws/go-moda/logger"
 
@@ -23,7 +27,6 @@ var importCmd = &cobra.Command{
 
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// TODO:flg to conf
-		logger.Infow("import cmd")
 		qdrantAddr, err := cmd.Flags().GetString(qdrantFlag)
 		if err != nil {
 			return err
@@ -31,15 +34,35 @@ var importCmd = &cobra.Command{
 		qdrantClient := qdrant.NewQdrantClient(qdrantAddr)
 		defer qdrantClient.Close()
 
+		// TODO flg to conf apikey,proxy
+		aiClient, err := ai.NewAiClient("", "")
+		if err != nil {
+			return err
+		}
+
+		// TODO  collection,size use flg
 		if err = qdrantClient.CreateCollection("kubernetes", 1536); err != nil {
 			return err
 		}
-		// TODO config to QA
-		var qas []*QA
+
+		// TODO flg json file to QA
+		// TODO flg json string to QA
+		dataFilePath, err := cmd.Flags().GetString(dataFileFlag)
+		if err != nil {
+			return err
+		}
+		qas, err := convertToQAs(dataFilePath)
+		if err != nil {
+			return err
+		}
 		points := []*pb.PointStruct{}
-		for _, qa := range qas {
-			// TODO openai CreateEmbeddings
-			var embedding []float32
+		qpsLenth := len(qas)
+		for i, qa := range qas {
+			embedding, err := aiClient.SimpleGetVec(qa.Questions)
+			if err != nil {
+				logger.Errorw("SimpleGetVec", "err", err, "question", qa.Questions, "index", i, "total", qpsLenth)
+				return err
+			}
 			point := buildPoint(qa.Questions, qa.Answers, embedding)
 			points = append(points, point)
 		}
@@ -72,4 +95,23 @@ func buildPoint(question string, answers string, embedding []float32) *pb.PointS
 	ret["answers"] = &pb.Value{Kind: &pb.Value_StringValue{StringValue: answers}}
 	point.Payload = ret
 	return point
+}
+
+func convertToQAs(dataFilePath string) ([]*QA, error) {
+	f, err := os.Open(dataFilePath)
+	if err != nil {
+		return nil, err
+	}
+	// TODO :big file handle
+	defer f.Close()
+	content, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	var qas []*QA
+	err = json.Unmarshal(content, qas)
+	if err != nil {
+		return nil, err
+	}
+	return qas, nil
 }
---
+++ pkg/ai/message.go
@@ -0,0 +1,66 @@
+package ai
+
+import (
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type (
+	ChatMessages []*ChatMessage
+	ChatMessage  struct {
+		Msg openai.ChatCompletionMessage
+	}
+)
+
+const (
+	RoleUser      = "user"
+	RoleAssistant = "assistant"
+	RoleSystem    = "system"
+)
+
+func (cm *ChatMessages) Clear() {
+	*cm = make([]*ChatMessage, 0)
+}
+
+func InitChatMessages() ChatMessages {
+	cm := make(ChatMessages, 0)
+	// cm.AddForSystem("You are a helpful K8S assistant. Use the provided text to form your answer. Keep your answer within 10 sentences. Accurate, helpful, concise and to the point")
+	cm.AddForSystem("You are a helpful K8S assistant. I will provide you with text, including the question title or keywords, question description, and reference answer. If I provide a reference answer, please try to use it as much as possible.Try to answer in Chinese as much as possible, Keep your answer within 10 sentences. Accurate, useful, concise and to the point")
+	return cm
+}
+
+func (cm *ChatMessages) AddFor(msg string, role string) {
+	*cm = append(*cm, &ChatMessage{
+		Msg: openai.ChatCompletionMessage{
+			Role:    role,
+			Content: msg,
+		},
+	})
+}
+
+func (cm *ChatMessages) AddForAssistant(msg string) {
+	cm.AddFor(msg, RoleAssistant)
+}
+
+func (cm *ChatMessages) AddForSystem(msg string) {
+	cm.AddFor(msg, RoleSystem)
+}
+
+func (cm *ChatMessages) AddForUser(msg string) {
+	cm.AddFor(msg, RoleUser)
+}
+
+func (cm *ChatMessages) ToMessage() []openai.ChatCompletionMessage {
+	ret := make([]openai.ChatCompletionMessage, len(*cm))
+	for index, c := range *cm {
+		ret[index] = c.Msg
+	}
+	return ret
+}
+
+func (cm *ChatMessages) GetLast() string {
+	if len(*cm) == 0 {
+		return "nothing"
+	}
+
+	return (*cm)[len(*cm)-1].Msg.Content
+}
